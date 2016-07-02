138ab8c2f863e83ca472d95637987f962ab26588
blazehalderman <blazerboy123@gmail.com>
2016-07-02 14:05:24 -0700

    Added new Project + eyecon project

---
+++ Javascript-exercises/Eyecon-Proj/Eyecon-signin/Eyecon-game-login.js
@@ -0,0 +1,13 @@
+var User = function() {
+  this.email;
+  this.password;
+}
+
+var Users = [];
+
+var Login = document.getElementById('submit_button');
+
+
+Login.addEventListener('click', function() {
+  //add next page
+});
